05a270e200b636691231cd3fcd8f5d3f24f7d199
Maykon Henrique dos Santos <maykonhs@gmail.com>
2013-12-12 08:59:34 -0200

    commit

--- src/br/com/lojapp/pizzadelivery/fragments/FragmentActionBar.java
+++ src/br/com/lojapp/pizzadelivery/fragments/FragmentActionBar.java
@@ -1,5 +1,22 @@
 package br.com.lojapp.pizzadelivery.fragments;
 
-public class FragmentActionBar {
+import com.lojapp.pizzadelivery.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentActionBar extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View view = inflater.inflate(R.layout.fragment_actionbar, null);
+
+		return view;
+	}
 
 }
--- src/br/com/lojapp/pizzadelivery/fragments/FragmentFooter.java
+++ src/br/com/lojapp/pizzadelivery/fragments/FragmentFooter.java
@@ -1,5 +1,22 @@
 package br.com.lojapp.pizzadelivery.fragments;
 
-public class FragmentFooter {
+import com.lojapp.pizzadelivery.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentFooter extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View view = inflater.inflate(R.layout.fragment_footer, null);
+
+		return view;
+	}
 
 }
